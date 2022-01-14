c4260dbb4d5f6644a7b98ee7ef1b65e5284b7d8b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-01-14 08:38:46 -0600

    simplified PlayerEntry

--- lib/player_entry.dart
+++ lib/player_entry.dart
@@ -2,16 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'player_state.dart';
 
-// This needs to be stateful in order
-// to hold a TextEditingController instance.
-class PlayerEntry extends StatefulWidget {
+class PlayerEntry extends StatelessWidget {
   const PlayerEntry({Key? key}) : super(key: key);
 
-  @override
-  _PlayerEntryState createState() => _PlayerEntryState();
-}
-
-class _PlayerEntryState extends State<PlayerEntry> {
   @override
   Widget build(BuildContext context) {
     final playerState = Provider.of<PlayerState>(context, listen: false);
